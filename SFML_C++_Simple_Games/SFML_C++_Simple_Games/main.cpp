a5d6adab6573cfcd1513c18e7077e652bf53a555
Headturna <suraj-sharma@live.com>
2019-08-03 16:08:24 +0200

    Linked to SFML in video

--- SFML_C++_Simple_Games/SFML_C++_Simple_Games/main.cpp
+++ SFML_C++_Simple_Games/SFML_C++_Simple_Games/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
 int main()
 {
+	std::cout << "Hello from SFML!" << "\n";
+	
+	system("pause");
 	return 0;
-
 }
