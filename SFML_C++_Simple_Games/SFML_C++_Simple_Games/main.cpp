78a65441fee7b8adce53f7c5f4ba15b71af68854
Headturna <suraj-sharma@live.com>
2019-08-04 20:00:40 +0200

    Added window

--- SFML_C++_Simple_Games/SFML_C++_Simple_Games/main.cpp
+++ SFML_C++_Simple_Games/SFML_C++_Simple_Games/main.cpp
@@ -6,10 +6,43 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
+//using namespace sf;
+
 int main()
 {
-	std::cout << "Hello from SFML!" << "\n";
-	
-	system("pause");
+	//Window
+	sf::RenderWindow window(sf::VideoMode(640, 480), "My first game", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event ev;
+
+	//Game loop
+	while (window.isOpen())
+	{
+		//Event polling
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type)
+			{
+			case sf::Event::Closed: 
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+					window.close();
+				break;
+			}
+		}
+
+		//Update
+
+
+		//Render
+		window.clear(sf::Color::Blue); //Clear old frame
+
+		//Draw your game
+
+		window.display(); //Tell app that window is done drawing
+	}
+
+	//End of application
 	return 0;
 }
