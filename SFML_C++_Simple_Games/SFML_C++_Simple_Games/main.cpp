11417de846bf50c1f73b76ca1904e063804d29ee
Headturna <suraj-sharma@live.com>
2020-04-24 12:00:01 +0200

    Started Game 4 Ended Game 3

--- SFML_C++_Simple_Games/SFML_C++_Simple_Games/main.cpp
+++ SFML_C++_Simple_Games/SFML_C++_Simple_Games/main.cpp
@@ -1,13 +1,14 @@
-#include "Game.h"
+#include<cstdlib>
+#include<ctime>
+#include<iostream>
 #include<time.h>
+
 
 int main()
 {
 	srand(time(static_cast<unsigned>(0)));
 
-	Game game;
-
-	game.run();
+	std::cout << "Hello from Game 4!" << "\n";
 
 	//End of application
 	return 0;
